bc8d4fa38c785d08073d24092bd418253889e92e
jefby <jef199006@gmail.com>
2014-07-02 23:18:45 +0800

    add findnsum.cpp

---
+++ source/findnsum.cpp
@@ -0,0 +1,42 @@
+/*
+	寻找和为m的任意组合
+*/
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+list<int>list1;
+void find_factor(int sum,int n)
+{
+	//递归出口
+	if(n <= 0 || sum <= 0)
+		return;
+	//输出找到的结果
+	if(sum == n)
+	{
+		//反转list1
+		list1.reverse();
+		for(list<int>::iterator iter = list1.begin();iter != list1.end();++iter)
+			cout << *iter << "+";
+		cout<<n<<endl;
+		list1.reverse();
+	}
+	list1.push_front(n);//典型的01背包问题
+	find_factor(sum-n,n-1);//放n,n-1个数填满sum-n
+	list1.pop_front();
+	find_factor(sum,n-1);//不放n,n-1个数填满sum
+}
+int main()
+{
+	int sum = 0, n = 0;
+	cout<<"请输入要等于多少的数值sum:"<<endl;
+	cin>>sum;
+	cout<<"请输入从1...n数列取值的n:"<<endl;
+	cin>>n;
+	find_factor(sum,n);
+	fflush(stdin);
+	getchar();
+
+	return 0;
+}
